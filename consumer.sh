09e648bec3e4d2d0a76192d1fdaec386fe1bc4c1
Florique <95179214+Floriquek@users.noreply.github.com>
2022-02-07 13:35:47 +0200

    ..

--- consumer.sh
+++ consumer.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]
+then
+        echo "provide arguments: ./script.sh KAFKA_ARG"
+fi
+
 kubectl exec $1 -ti -- bash /kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka-1:9092 --topic test1
