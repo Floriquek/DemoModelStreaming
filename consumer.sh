06160ceb2dd7cdeae6dcb461beddc405b12da44a
Florique <95179214+Floriquek@users.noreply.github.com>
2022-02-07 13:36:04 +0200

    ..

--- consumer.sh
+++ consumer.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]
 then
-        echo "provide arguments: ./script.sh KAFKA_ARG"
+        echo "provide arguments: ./consumer.sh KAFKA_ARG"
 fi
 
 kubectl exec $1 -ti -- bash /kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka-1:9092 --topic test1
